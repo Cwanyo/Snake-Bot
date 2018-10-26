5d89c87b59cf7bccf3d9decda9f0e5bdee347606
Cwanyo <manwan_444@hotmail.com>
2018-10-26 23:27:57 +0700

    init

---
+++ Main.py
@@ -0,0 +1,65 @@
+import pygame
+from Snake import Snake
+from Food import Food
+
+WINDOW_WIDTH = 20  # 30
+WINDOW_HEIGHT = 20
+PIXEL_SIZE = 20
+
+
+def main():
+    window = pygame.display.set_mode((WINDOW_WIDTH * PIXEL_SIZE, WINDOW_HEIGHT * PIXEL_SIZE))
+    pygame.display.set_caption("SNAKE GAME")
+    fps = pygame.time.Clock()
+
+    snake = Snake(WINDOW_WIDTH, WINDOW_HEIGHT, PIXEL_SIZE, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+    food = Food(WINDOW_WIDTH, WINDOW_HEIGHT, PIXEL_SIZE)
+
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    snake.change_direction(snake.moves[0])
+                    break
+                elif event.key == pygame.K_DOWN:
+                    snake.change_direction(snake.moves[1])
+                    break
+                elif event.key == pygame.K_LEFT:
+                    snake.change_direction(snake.moves[2])
+                    break
+                elif event.key == pygame.K_RIGHT:
+                    snake.change_direction(snake.moves[3])
+                    break
+
+        window.fill((0, 0, 0))
+
+        snake.move()
+
+        if snake.collision_food(food.location):
+            print('food')
+            food.state = False
+
+        food.spawn(snake)
+
+        if snake.collision_obstacles():
+            print('over')
+            run = False
+
+        if snake.get_length() == WINDOW_WIDTH * WINDOW_HEIGHT:
+            print('win')
+            run = False
+
+        food.render(window)
+        snake.render(window)
+        pygame.display.update()
+
+        fps.tick(5)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Snake.py
@@ -0,0 +1,68 @@
+import pygame
+
+
+class Snake:
+    def __init__(self, window_width, window_height, pixel_size, init_x, init_y):
+        self.window_width = window_width
+        self.window_height = window_height
+        self.pixel_size = pixel_size
+
+        self.moves = [[0, -1],  # up
+                      [0, 1],  # down
+                      [-1, 0],  # right
+                      [1, 0]]  # left
+
+        self.head_color = (0, 100, 0)
+        self.body_color = (0, 200, 0)
+
+        self.head = [init_x, init_y]
+        self.body = [[self.head[0], self.head[1] + 1]]
+        # for i in range(3, 97):
+        #     self.body.append([self.head[0], self.head[1] + i])
+
+        self.moveDirection = self.moves[0]
+
+    def change_direction(self, direction):
+        if direction == self.moves[0] and not self.moveDirection == self.moves[1]:
+            self.moveDirection = direction
+        elif direction == self.moves[1] and not self.moveDirection == self.moves[0]:
+            self.moveDirection = direction
+        elif direction == self.moves[2] and not self.moveDirection == self.moves[3]:
+            self.moveDirection = direction
+        elif direction == self.moves[3] and not self.moveDirection == self.moves[2]:
+            self.moveDirection = direction
+
+    def move(self):
+        self.body.insert(0, self.head)
+        self.body.pop()
+        self.head = [self.head[0] + self.moveDirection[0], self.head[1] + self.moveDirection[1]]
+
+    def collision_food(self, food_location):
+        if self.head == food_location:
+            self.body.insert(-1, self.body[-1])
+            return True
+        else:
+            return False
+
+    def collision_obstacles(self):
+        if self.head[0] < 0 or self.head[0] >= self.window_width \
+                or self.head[1] < 0 or self.head[1] >= self.window_height:
+            return True
+
+        for b in self.body:
+            if self.head == b:
+                return True
+
+        return False
+
+    def get_length(self):
+        return len(self.body) + 1
+
+    def render(self, win):
+        for b in self.body:
+            pygame.draw.rect(win, self.body_color,
+                             (b[0] * self.pixel_size, b[1] * self.pixel_size, self.pixel_size, self.pixel_size))
+
+        pygame.draw.rect(win, self.head_color,
+                         (self.head[0] * self.pixel_size, self.head[1] * self.pixel_size, self.pixel_size,
+                          self.pixel_size))
